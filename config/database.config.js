d2896d2894c64a3a9412cd4cf4f3c8fdd1da9282
vaibhavmorye <moryevaibhav93@gamil.com>
2018-04-27 00:56:37 +0530

    added mongo cluster

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    url:'mongodb+srv://vaibhavmorye:qwerty123@cluster0-c74rs.mongodb.net/easy-notes'
+    //url: 'mongodb://localhost:27017/easy-notes'
 }
